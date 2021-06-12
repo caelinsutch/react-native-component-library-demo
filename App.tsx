6733a8ce41e9ea9bdd3fc1b5317cbe04fc226e4b
Caelin Sutch <caelinsutch@gmail.com>
2021-06-12 11:32:31 -0700

    Create components

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import {ThemeProvider} from '@shopify/restyle';
+import React from "react";
+import { ThemeProvider } from "@shopify/restyle";
 
-import Storybook from './storybook';
-import theme from './src/theme';
+import Storybook from "./storybook";
+import theme from "./src/theme";
 
 const App = () => (
   <ThemeProvider theme={theme}>
-    <Storybook/>
+    <Storybook />
   </ThemeProvider>
 );
 
---
+++ src/Components/Button.tsx
@@ -0,0 +1,42 @@
+import { BoxProps, TextProps } from "@shopify/restyle";
+import { Theme } from "../theme";
+import { ActivityIndicator, TouchableHighlight } from "react-native";
+import Box from "./Box";
+import Text from "./Text";
+
+type ButtonProps = {
+  onPress: () => void;
+  loading?: boolean;
+  textProps?: TextProps<Theme>;
+} & Partial<BoxProps<Theme>>;
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  onPress,
+  loading,
+  textProps,
+  ...props
+}) => (
+  <TouchableHighlight underlayColor="transparent" onPress={onPress}>
+    <Box
+      py="m"
+      px="xl"
+      backgroundColor="primary"
+      shadowOffset={{ height: 2, width: 0 }}
+      shadowRadius={5}
+      shadowColor="black"
+      shadowOpacity={0.2}
+      {...props}
+    >
+      {loading ? (
+        <ActivityIndicator color="white" />
+      ) : (
+        <Text color="white" {...textProps}>
+          {children}
+        </Text>
+      )}
+    </Box>
+  </TouchableHighlight>
+);
+
+export default Button;
